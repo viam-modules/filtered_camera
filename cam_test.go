5556294a8237f3f6a618984cc9712a99aa104fb4
Jalen Geason <jalen.geason@viam.com>
2025-04-28 19:02:32 -0400

    [RSDK-8918] - Changes to filtered-camera to adhere to checklist

--- cam_test.go
+++ cam_test.go
@@ -7,10 +7,13 @@ import (
 	"time"
 
 	"github.com/viam-modules/filtered_camera/image_buffer"
+	"go.viam.com/rdk/components/camera"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/rimage"
 	"go.viam.com/rdk/services/vision"
 	"go.viam.com/rdk/testutils/inject"
+	"go.viam.com/rdk/utils"
 	"go.viam.com/rdk/vision/classification"
 	"go.viam.com/rdk/vision/objectdetection"
 
@@ -153,4 +156,138 @@ func TestWindow(t *testing.T) {
 	test.That(t, b, test.ShouldEqual, fc.buf.ToSend[0].Meta.CapturedAt)
 	test.That(t, a, test.ShouldEqual, fc.buf.ToSend[1].Meta.CapturedAt)
 
+}
+
+func TestValidate(t *testing.T) {
+	conf := &Config{
+		Classifications: map[string]float64{"a": .8},
+		Objects:         map[string]float64{"b": .8},
+		WindowSeconds:   10,
+	}
+
+	res, err := conf.Validate(".")
+	test.That(t, res, test.ShouldBeNil)
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "\"camera\" is required")
+	
+	conf.Camera = "foo"
+	res, err = conf.Validate(".")
+	test.That(t, res, test.ShouldBeNil)
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "\"vision\" is required")
+
+	conf.Vision = "foo"
+	res, err = conf.Validate(".")
+	test.That(t, res, test.ShouldNotBeNil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldResemble, []string{"foo", "foo"})
+}
+
+func TestImage(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+
+	fc := &filteredCamera{
+		conf: &Config{
+			Classifications: map[string]float64{"a": .8},
+			Objects:         map[string]float64{"b": .8},
+			WindowSeconds:   10,
+		},
+		logger: logger,
+		vis:    getDummyVisionService(),
+		buf:    imagebuffer.ImageBuffer{},
+		cam: &inject.Camera{
+			ImagesFunc: func(ctx context.Context) ([]camera.NamedImage, resource.ResponseMetadata, error) {
+				return []camera.NamedImage{
+					{Image: a, SourceName: ""},
+					{Image: b, SourceName: ""},
+					{Image: c, SourceName: ""},
+				}, resource.ResponseMetadata{}, nil
+			},
+		},
+	}
+
+	ctx := context.Background()
+
+	res, meta, err := fc.Image(ctx, utils.MimeTypeJPEG, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldNotBeNil)
+
+	decodedImage, err := rimage.EncodeImage(ctx, a, utils.MimeTypeJPEG)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldResemble, decodedImage)
+
+	test.That(t, meta, test.ShouldNotBeNil)
+	test.That(t, meta.MimeType, test.ShouldResemble, utils.MimeTypeJPEG)
+}
+
+func TestImages(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+
+	namedImages := []camera.NamedImage{
+		{Image: a, SourceName: ""},
+		{Image: b, SourceName: ""},
+		{Image: c, SourceName: ""},
+	}
+
+	timestamp := time.Now()
+
+	fc := &filteredCamera{
+		conf: &Config{
+			Classifications: map[string]float64{"a": .8},
+			Objects:         map[string]float64{"b": .8},
+			WindowSeconds:   10,
+		},
+		logger: logger,
+		vis:    getDummyVisionService(),
+		buf:    imagebuffer.ImageBuffer{},
+		cam: &inject.Camera{
+			ImagesFunc: func(ctx context.Context) ([]camera.NamedImage, resource.ResponseMetadata, error) {
+				return namedImages, resource.ResponseMetadata{CapturedAt: timestamp}, nil
+			},
+		},
+	}
+
+	ctx := context.Background()
+
+	res, meta, err := fc.Images(ctx)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldNotBeNil)
+
+	test.That(t, len(res), test.ShouldEqual, 3)
+	test.That(t, res, test.ShouldResemble, namedImages)
+
+	test.That(t, meta, test.ShouldNotBeNil)
+	test.That(t, meta.CapturedAt, test.ShouldResemble, timestamp)
+}
+
+func TestProperties(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+
+	properties := camera.Properties{
+		SupportsPCD:    false,
+		ImageType:    	camera.ImageType("color"),
+		MimeTypes:  	[]string{utils.MimeTypeJPEG},
+	}
+
+	fc := &filteredCamera{
+		conf: &Config{
+			Classifications: map[string]float64{"a": .8},
+			Objects:         map[string]float64{"b": .8},
+			WindowSeconds:   10,
+		},
+		logger: logger,
+		vis:    getDummyVisionService(),
+		buf:    imagebuffer.ImageBuffer{},
+		cam: &inject.Camera{
+			PropertiesFunc: func(ctx context.Context) (camera.Properties, error) {
+				return properties, nil
+			},
+		},
+	}
+
+	ctx := context.Background()
+
+	res, err := fc.Properties(ctx)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldResemble, properties)
 }
