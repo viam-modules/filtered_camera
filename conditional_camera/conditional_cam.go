496b11a156e444ea17240080e98fd30ca2ddaa47
Alan Davidson <alan@viam.com>
2025-06-09 14:17:14 -0400

    add comment explaining conditional-camera

--- conditional_camera/conditional_cam.go
+++ conditional_camera/conditional_cam.go
@@ -1,3 +1,6 @@
+// The filtered camera uses a vision service (classifier or detector) to decide whether images are
+// interesting enough to get through the filter, whereas the confitional camera sends a DoCommand
+// to any generic resource, and uses that response to decide whether to let images through.
 package conditional_camera
 
 import (
